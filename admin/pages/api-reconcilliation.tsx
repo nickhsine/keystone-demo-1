b1ef396caca803fb5b77c85cb1149fab246b6bda
Greg Morris <greg.morris@carma.earth>
2023-05-16 20:15:22 +0100

    fixed fetcher

--- admin/pages/api-reconcilliation.tsx
+++ admin/pages/api-reconcilliation.tsx
@@ -3,11 +3,40 @@ import { Heading } from '@keystone-ui/core';
 import useSWR from 'swr'
 import { AppResponse, AppResults, AppOffer, Promotion, PromotionData, PromoCategory, PromoSplitDetails, TableProps, ApiArray, ApiArrayItem } from '../interfaces/reconcilliation';
 import ApiTable from '../components/api-table';
+import {useMemo} from "react";
 
 const bubbleOffersUrl = process.env.NEXT_PUBLIC_CARMA_APP_OFFERS_API_URL ?? 'i do not exist';
+const networkBOffersUrl = "https://localhost:3030/promotions" ?? '';
 const bubbleOffersToken = process.env.NEXT_PUBLIC_CARMA_APP_API_BEARER_TOKEN ?? '';
-const networkBOffersUrl = "http://localhost:3030/promotions" ?? '';
 const networkBOffersToken = process.env.NEXT_PUBLIC_NETWORKB_BEARER_TOKEN ?? '';
+
+
+interface RequestParams {
+    token: string,
+    method: string
+}
+
+const fetcher = ({target: url, token, method} :{target: string, token: string, method: string}) => {
+
+    const requestHeaders = new Headers();
+    
+    if (token) {
+        requestHeaders.append("Authorization", token) 
+    }
+
+    const requestOptions: RequestInit = {
+        method: method,
+        headers: requestHeaders,
+        redirect: 'follow'
+      };
+
+
+    
+    return (
+        fetch(url, requestOptions).
+        then(r => r.json())
+    )
+}
 
 
 
@@ -23,30 +52,34 @@ export default function BubblePromotions() {
 
 function ApiDisplay() {
 
-    const bubbleData = callAPI(bubbleOffersUrl, bubbleOffersToken, "GET")
-    console.log(bubbleData)
-    //REVERT AFTER
-    const nbData = callAPI(networkBOffersUrl, networkBOffersToken, "GET")
+    const {data: bubbleData, error: bubbleError, isLoading: bubbleIsLoading} = callAPI<AppResponse>(bubbleOffersUrl, bubbleOffersToken, "GET")
+    const {data: nbData, error: nBError, isLoading: nBIsLoading} = callAPI<Promotion[]>(networkBOffersUrl, null, "GET")
 
     const combinedArray = getArrData(bubbleData, nbData)
 
+    console.log("data", bubbleData?.response.results)
+
+
     return (
         <>
-        <p>DISPLAY</p>
-            {combinedArray && <ApiTable tableProps={combinedArray.dataArr}></ApiTable>}
-            {/* {combinedArray && <ApiTable tableProps={combinedArray.singleArr}></ApiTable>} */}
+        <ErrorOrLoading
+            error = {bubbleError || nBError}
+            isLoading = {bubbleIsLoading || nBIsLoading}
+            isResponseData = {bubbleData || nbData}
+        />
+        {combinedArray && <ApiTable tableProps={combinedArray.dataArr}></ApiTable>}
+        {/* {combinedArray && <ApiTable tableProps={combinedArray.singleArr}></ApiTable>} */}
         </>
     )
 }
 
-const getArrData = (bubbleData: AppResponse | void | undefined, nbData:  Promotion[] | void | undefined) => {
+const getArrData = (bubbleData: AppResponse | null | undefined, nbData:  Promotion[] | null | undefined) => {
     if (!bubbleData || !nbData) {
-        console.log("no data")
         return null;
     }
     const dataArr: ApiArray[] = []
     const singleArr: ApiArray[] = []
-    console.log(bubbleData)
+
     bubbleData.response.results.map((offer, i: number) => 
         dataArr.push({Id: offer.nimda_id_number, items: [{"Name": offer.name_text, "Discount": offer.discount_percentage_number ? offer.discount_percentage_number.toFixed(2)+"%" : "£"+offer.discount_amount_number, "Expires": offer.expiry_date_date.split('T')[0], Source: "Bubble"} ]}),
     )
@@ -68,32 +101,30 @@ const getArrData = (bubbleData: AppResponse | void | undefined, nbData:  Promoti
                 })
         })
     )
-    console.log(dataArr)
     return (
         {dataArr}
     );
 
 }
-const callAPI = (target: string, token: string, method: string) => {
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", token);
+const callAPI = <T,>(target: string, token: string | null, method: string) => {
+    const { data, error, isLoading} = useSWR<T>({target, token, method}, fetcher)
+    return (
+        {data, error, isLoading}
+    )
+    
+}
 
-    const requestOptions: RequestInit = {
-        method: method,
-        headers: myHeaders,
-        redirect: 'follow',
-    };
-
-    fetch(target, requestOptions)
-    .then(response => response.json())
-    .then(result => {
-        return (
-            console.log(result)
-        )
-    })
-    .catch(error => {
-        return (
-            error
-        )
-    })
+const ErrorOrLoading = (error: any, isLoading: boolean, isResponseData: boolean) => {
+    if (error[0] != undefined) return (
+        <div>Error</div>
+    )
+    if (isLoading == true) return (
+        <div>Loading...</div>
+    )
+    if (isResponseData == false) return (
+        <div>Failed to load</div>
+    )
+    return (
+        null
+    )
 }
